f5ee85a318981119b81b488a21fdf43374e1b0ab
nguyenhieuhoc20 <nhocpro2301@gmail.com>
2022-04-27 09:46:11 +0700

    nopkiemtra

---
+++ js/modal.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    let i = 2; // Khởi tạo số thứ tự
+    $("#btnDK").click(function() {
+        $("#myModal").modal();
+    })
+    function kiemtrama() {
+        let maukt = /^[0-9]{10}$/;
+        let chuoi = $("#txtMASV").val();
+        if (chuoi == "") {
+          $("#tbMASV").html("Bắt buộc nhập");
+          return false;
+        } else if (!maukt.test(chuoi)) {
+          $("#tbMASV").html("Nhập sai, chỉ dùng 10 chữ số");
+          return false;
+        } else {
+          $("#tbMASV").html("*");
+          return true;
+        }
+      }
+      $("#txtMASV").blur(kiemtrama);
+    
+     // Kiểm tra họ tên
+    function kiemtrahoten() {
+        let chuoi = $("#txtHT").val();
+        let maukt = /([A-Z]{1}[a-z]+)((\s{1}[A-Z]{1}[a-z]+){1,})$/;
+        if (chuoi == "") {
+            $("#tbHoTen").html("Bắt buộc nhập");
+            return false;
+        } else if (!maukt.test(chuoi)) {
+            $("#tbHoTen").html("Ký tự đầu tiên viết hoa, không sử dụng số");
+            return false;
+        } else {
+            $("#tbHoTen").html("*");
+            return true;
+        }
+    }
+    $("#txtHT").blur(kiemtrahoten);
+     // Kiểm tra email
+    function kiemtraemail() {
+        let chuoi = $("#txtEmail").val();
+        let maukt = /^\w+([\.-]?w+)*@iuh.edu.vn$/;
+        if (chuoi == "") {
+            $("#tbEmail").html("Bắt buộc nhập");
+            return false;
+        } else if (!maukt.test(chuoi)) {
+            $("#tbEmail").html("Nhập sai mẫu, nhập theo mẫu xxxxx@iuh.edu.vn");
+        } else {
+            $("#tbEmail").html("*");
+            return true;
+        }
+    }
+    $("#txtEmail").blur(kiemtraemail);
+});
